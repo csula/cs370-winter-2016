bb568756df0dd0492ef3b4595d92d0d814e84c87
John Tran <johnjtran@icloud.com>
2016-01-12 20:13:42 -0800

    added thread-demo
    source: https://computing.llnl.gov/tutorials/pthreads/

--- src/program.c
+++ src/program.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main(int argc, char *argv[])
 {
   int N = 0, z = 0, k = 0, M = 0;
   if (argc < 4) {
-    printf("usage: %s <N> <z> <k> <M>\n");
+    printf("usage: %s <N> <z> <k> <M>\n", argv[0]);
     return 0;
   }
   N = atoi(argv[1]);
